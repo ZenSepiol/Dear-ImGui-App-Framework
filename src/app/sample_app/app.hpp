2110b9240b2e15fe425e4357cf1404593fa563ef
zen <zen@Zen.myguest.virtualbox.org>
2023-03-05 12:49:46 +0100

    Create directory for all applications

---
+++ src/app/sample_app/app.hpp
@@ -0,0 +1,56 @@
+#include "app_base/app_base.hpp"
+#include "implot_internal.h"
+#include <iostream>
+#include <string>
+
+class App : public AppBase<App>
+{
+  public:
+    App(){};
+    virtual ~App() = default;
+
+    // Anything that needs to be called once OUTSIDE of the main application loop
+    void StartUp()
+    {
+    }
+
+    // Anything that needs to be called cyclically INSIDE of the main application loop
+    void Update()
+    {
+        ImGui::ShowDemoWindow();
+    }
+
+    // The callbacks are updated and called BEFORE the Update loop is entered
+    // It can be assumed that inside the Update loop all callbacks have already been processed
+    static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+    {
+        // For Dear ImGui to work it is necessary to queue if the mouse signal is already processed by Dear ImGui
+        // Only if the mouse is not already captured it should be used here.
+        ImGuiIO& io = ImGui::GetIO();
+        if (!io.WantCaptureMouse)
+        {
+        }
+    }
+
+    static void CursorPosCallback(GLFWwindow* window, double xpos, double ypos)
+    {
+        // For Dear ImGui to work it is necessary to queue if the mouse signal is already processed by Dear ImGui
+        // Only if the mouse is not already captured it should be used here.
+        ImGuiIO& io = ImGui::GetIO();
+        if (!io.WantCaptureMouse)
+        {
+        }
+    }
+
+    static void KeyCallback(GLFWwindow* window, int key, int scancode, int actions, int mods)
+    {
+        // For Dear ImGui to work it is necessary to queue if the keyboard signal is already processed by Dear ImGui
+        // Only if the keyboard is not already captured it should be used here.
+        ImGuiIO& io = ImGui::GetIO();
+        if (!io.WantCaptureKeyboard)
+        {
+        }
+    }
+
+  private:
+};
