13741f6396be114565dce260b306aa8468e400a7
Zen <ZenSepiol@pm.me>
2023-03-15 08:53:28 +0100

    First draft of game logic

--- src/app/tictactoe/tictactoe.hpp
+++ src/app/tictactoe/tictactoe.hpp
@@ -22,10 +22,6 @@ class TicTacToe : public AppBase<TicTacToe>
 
     void Update()
     {
-        ImGui::Begin("Tool");
-
-        ImGui::End();
-
         // ImDrawList* draw = ImGui::GetBackgroundDrawList();
 
         for (int row = 0; row < 3; ++row)
@@ -54,17 +50,28 @@ class TicTacToe : public AppBase<TicTacToe>
         ImVec2 center   = ImVec2(cell_width / 2 + top_left.x, cell_height / 2 + top_left.y);
         if (cell_value == CellState::Circle)
         {
-            draw->AddCircle(center, symbol_size / 2, ImGui::GetColorU32(ImGuiCol_Text));
+            if (game_over && winner == CellState::Circle)
+                draw->AddCircle(center, symbol_size / 2, ImGui::GetColorU32(ImVec4(0.50f, 0.00f, 0.00f, 1.00f)));
+            else
+                draw->AddCircle(center, symbol_size / 2, ImGui::GetColorU32(ImGuiCol_Text));
+
         }
         else if (cell_value == CellState::Cross)
         {
             ImVec2 tl(center.x - symbol_size / 2, center.y - symbol_size / 2);
             ImVec2 br(center.x + symbol_size / 2, center.y + symbol_size / 2);
-            draw->AddLine(tl, br, ImGui::GetColorU32(ImGuiCol_Text));
+            if (game_over && winner == CellState::Cross)
+                draw->AddLine(tl, br, ImGui::GetColorU32(ImGui::GetColorU32(ImVec4(0.50f, 0.00f, 0.00f, 1.00f))));
+            else
+                draw->AddLine(tl, br, ImGui::GetColorU32(ImGuiCol_Text));
 
             ImVec2 tr(center.x + symbol_size / 2, center.y - symbol_size / 2);
             ImVec2 bl(center.x - symbol_size / 2, center.y + symbol_size / 2);
-            draw->AddLine(tr, bl, ImGui::GetColorU32(ImGuiCol_Text));
+            if (game_over && winner == CellState::Cross)
+                draw->AddLine(tr, bl, ImGui::GetColorU32(ImGui::GetColorU32(ImVec4(0.50f, 0.00f, 0.00f, 1.00f))));
+            else
+                draw->AddLine(tr, bl, ImGui::GetColorU32(ImGuiCol_Text));
+
         }
         else
         {
@@ -74,12 +81,72 @@ class TicTacToe : public AppBase<TicTacToe>
             cell_value == CellState::Empty)
         {
             draw->AddRect(top_left, bottom_right, ImGui::GetColorU32(ImGuiCol_Text), 0.0f, 0, 4.0);
+
+            if (clicked)
+                UpdateCellState(row, col);
+
         }
     }
 
     CellState GetCell(int row, int col)
     {
         return game_state[row][col];
+    }
+
+    void UpdateCellState(int row, int col)
+    {
+        if (game_over)
+            return;
+
+        if (GetCell(row, col) != CellState::Empty)
+            return;
+        
+        game_state[row][col] = current_player;
+
+        CheckForGameOver(row, col);
+        if (current_player == CellState::Cross)
+            current_player = CellState::Circle;
+        else
+            current_player = CellState::Cross;
+
+    }
+
+    void CheckForGameOver(int row, int col)
+    {
+        game_over = CheckDiag() || CheckRow(row) || CheckCol(col);
+        if (game_over)
+            winner = current_player;
+    }
+
+    bool CheckRow(int row)
+    {
+        bool over = true;
+        for (int i = 0; i < 3; ++i)
+        {
+            if (!(GetCell(row, i) == current_player))
+                over = false;
+        }
+        return over;
+    }
+
+    bool CheckCol(int col)
+    {
+        bool over = true;
+        for (int i = 0; i < 3; ++i)
+        {
+            if (!(GetCell(i, col) == current_player))
+                over = false;
+        }
+        return over;
+    }
+
+    bool CheckDiag()
+    {
+        if (GetCell(0,0) == current_player && GetCell(1,1) == current_player && GetCell(2,2) == current_player)
+            return true;
+        if (GetCell(2,0) == current_player && GetCell(1,1) == current_player && GetCell(0,2) == current_player)
+            return true;
+        return false; 
     }
 
     static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
@@ -130,6 +197,10 @@ class TicTacToe : public AppBase<TicTacToe>
     static int mouse_y;
 
     static bool clicked;
+
+    CellState current_player = CellState::Cross;
+    CellState winner = CellState::Empty;
+    bool game_over = false;
 };
 
 int TicTacToe::mouse_x = 0;
